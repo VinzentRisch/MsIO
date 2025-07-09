c17bc829b23adfa1171ed253e39793ddcd636f7a
VinzentRisch <risch.vinzent@gmail.com>
2025-07-09 10:33:47 +0200

    removed everything related to alabaster

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,5 @@
-.is_alabaster_matrix_installed <- function() {
-    requireNamespace("alabaster.matrix", quietly = TRUE)
-}
-
 .is_spectra_installed <- function() {
     requireNamespace("Spectra", quietly = TRUE)
-}
-
-.is_alabaster_se_installed <- function() {
-    requireNamespace("alabaster.se", quietly = TRUE)
 }
 
 .is_ms_experiment_installed <- function() {
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,13 +1,5 @@
-test_that(".is_alabaster_matrix_installed works", {
-    expect_true(.is_alabaster_matrix_installed())
-})
-
 test_that(".is_spectra_installed works", {
     expect_true(.is_spectra_installed())
-})
-
-test_that(".is_alabaster_se_installed works", {
-    expect_true(.is_alabaster_se_installed())
 })
 
 test_that(".is_ms_experiment_installed works", {
